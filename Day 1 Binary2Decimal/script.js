ce8aed6cfbc02f37c7e513504bf54adb4ad30bb0
Eduardo Secco <dudssecco.code@gmail.com>
2023-10-17 18:57:26 -0300

    Update

---
+++ Day 1 Binary2Decimal/script.js
@@ -0,0 +1,49 @@
+function BtwoD(){
+
+    if(document.getElementById('ibinario').value == "" 
+    && document.getElementById('idecimal').value == ""){
+        window.alert("Fill in one of the fields")
+    } else 
+    if(document.getElementById('ibinario').value != "" 
+    && document.getElementById('idecimal').value != ""){
+        window.alert("Fill in only one of the fields")
+    }
+
+    if(document.getElementById('ibinario').value != "" 
+    && document.getElementById('idecimal').value == ""){
+
+        
+
+    } else {
+
+        
+
+    }
+
+}
+
+function DtwoH(){
+
+    if(document.getElementById('idecimal2').value == "" 
+    && document.getElementById('ihexa').value == ""){
+        window.alert("Fill in one of the fields")
+    } else 
+    if(document.getElementById('idecimal2').value != "" 
+    && document.getElementById('ihexa').value != ""){
+        window.alert("Fill in only one of the fields")
+    }
+
+}
+
+function BtwoH(){
+
+    if(document.getElementById('ibinario2').value == "" 
+    && document.getElementById('ihexa2').value == ""){
+        window.alert("Fill in one of the fields")
+    } else 
+    if(document.getElementById('ibinario2').value != "" 
+    && document.getElementById('ihexa2').value != ""){
+        window.alert("Fill in only one of the fields")
+    }
+
+}
