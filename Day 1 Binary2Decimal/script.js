5244e38680beb2ed8822e9b88c79d53080a5b48c
Eduardo Secco <dudssecco.code@gmail.com>
2023-10-17 22:43:37 -0300

    Update Script

--- Day 1 Binary2Decimal/script.js
+++ Day 1 Binary2Decimal/script.js
@@ -51,18 +51,26 @@ function DtwoH(){
     if(document.getElementById('idecimal2').value != "" 
     && document.getElementById('ihexa').value == ""){
 
-        
+        let numD = parseInt(document.getElementById('idecimal2').value)
+        let numH = numD.toString(16)
+
+        document.getElementById('ihexa').value = numH
         
     } else if(document.getElementById('idecimal2').value == "" 
     && document.getElementById('ihexa').value != ""){
 
-        
+        let numH = document.getElementById('ihexa').value
+        let numD = parseInt(numH, 16)
+
+        document.getElementById('idecimal2').value = numD
 
     }
 
 }
 
 function BtwoH(){
+
+    // Verification
 
     if(document.getElementById('ibinario2').value == "" 
     && document.getElementById('ihexa2').value == ""){
@@ -71,6 +79,28 @@ function BtwoH(){
     if(document.getElementById('ibinario2').value != "" 
     && document.getElementById('ihexa2').value != ""){
         window.alert("Fill in only one of the fields")
+    }
+
+    // Convert
+
+    if(document.getElementById('ibinario2').value != "" 
+    && document.getElementById('ihexa2').value == ""){
+
+        let numB = document.getElementById('ibinario2').value
+        let numD = parseInt(numB, 2)
+        let numH = numD.toString(16)
+
+        document.getElementById('ihexa2').value = numH
+
+    } else if(document.getElementById('ibinario2').value == "" 
+    && document.getElementById('ihexa2').value != ""){
+
+        let numH = document.getElementById('ihexa2').value
+        let numD = parseInt(numH, 16)
+        let numB = numD.toString(2)
+
+        document.getElementById('ibinario2').value = numB
+
     }
 
 }
